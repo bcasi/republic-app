66230bcc1108c33d11a5219819b8ecda90c21a6d
arnetkc <arnetkc@gmail.com>
2024-09-25 11:09:01 +0530

    added video

--- frontend/src/components/company/CompanyLayout.jsx
+++ frontend/src/components/company/CompanyLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Container from "../common/Container";
 import Header from "../common/Header";
+import CompanyHeaderSection from "./CompanyHeaderSection";
 
 const CompanyLayout = () => {
   return (
     <div className="bg-white h-screen w-screen">
       <Header />
+      <Container>
+        <CompanyHeaderSection />
+      </Container>
     </div>
   );
 };
